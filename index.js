b23cc94ef407e37d43e01c779c4f6fa06dab82c6
MsDiala <diala-is-diala@hotmail.com>
2023-10-15 22:11:11 +0100

    done

---
+++ index.js
@@ -0,0 +1,40 @@
+// Function to add two numbers
+function add(a, b) {
+    return a + b;
+  }
+  
+  // Function to subtract one number from another
+  function subtract(a, b) {
+    return a - b;
+  }
+  
+  // Function to multiply two numbers
+  function multiply(a, b) {
+    return a * b;
+  }
+  
+  // Function to divide one number by another
+  function divide(a, b) {
+    return a / b;
+  }
+  
+  // Function to increment a number by one
+  function increment(n) {
+    return n + 1;
+  }
+  
+  // Function to decrement a number by one
+  function decrement(n) {
+    return n - 1;
+  }
+  
+  // Function to parse a string into a base-10 integer
+  function makeInt(string) {
+    return parseInt(string, 10);
+  }
+  
+  // Function to parse a string into a float
+  function preserveDecimal(string) {
+    return parseFloat(string);
+  }
+  
